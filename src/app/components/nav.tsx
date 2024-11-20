efc16e1e2832d12e159564ec93170682509c9f9b
Wen Chen <wc467@cornell.edu>
2024-11-20 12:47:16 -0500

    website it2

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 
-function Nav() {
+const Nav: React.FC = () => {
+  // const section2Ref = useRef<HTMLDivElement | null>(null);
+  // const section3Ref = useRef<HTMLDivElement | null>(null);
+
+  const scrollToSectionById = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="fixed right-0 top-0 h-screen text-base w-1/6 flex items-center justify-center bg-second">
       <div className="w-full text-center flex flex-col gap-y-10">
-        <p>Home</p>
-        <p>About</p>
-        <p>Work Experience</p>
-        <p>Resume</p>
+        <p onClick={() => scrollToSectionById("section-1")}>About</p>
+        <p onClick={() => scrollToSectionById("section-2")}>Work Experience</p>
+        <p onClick={() => scrollToSectionById("section-3")}>
+          Project Experience
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default Nav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
 import Nav from "./components/nav";
 import Image from "next/image";
 import Section1 from "./components/section1";
+import { FaJsSquare } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+
 export default function Home() {
   return (
     <div className="bg-main">
       <div className="flex flex-row">
-        <div className="flex w-5/6 flex-col justify-center gap-y-40">
+        <div
+          id="section-1"
+          className="flex w-5/6 flex-col justify-center gap-y-40"
+        >
           <Section1 />
-          <div className="flex items-center flex-col">
-            <div className="w-4/5 min-h-screen">
+          <div className="flex items-center flex-col mt-[5%]">
+            <div className="w-4/5 min-h-screen" id="section-2">
               <p className="text-[32px] text-center text-white font-sans font-bold">
                 Work Experience
               </p>
@@ -23,11 +32,45 @@ export default function Home() {
                   <p className="text-white text-[16px] text-center font-bold">
                     Skills
                   </p>
-                  
+                  <div className="flex gap-x-10">
+                    <FaJsSquare className="bg-yellow-500 text-[36px]" />
+                    <SiTypescript className="bg-blue-500 text-[36px]" />
+                    <FaHtml5 className="bg-orange-500 text-[36px]" />
+                    <FaCss3Alt className="bg-blue-600 text-[36px]" />
+                  </div>
                 </div>
                 <p className="text-white text-[16px] text-center">
                   Worked in the ELSYS division of Georgia Tech Research
                   Institute. Created and catered web interfaces for clients.
+                </p>
+              </div>
+            </div>
+            <div className="w-4/5 min-h-screen" id="section-3">
+              <p className="text-[32px] text-center text-white font-sans font-bold">
+                Project Experience
+              </p>
+              <div className="flex gap-x-10 mt-[5%]">
+                <div className="flex flex-col gap-y-4">
+                  <Image
+                    alt="experience 1"
+                    src={"/webdev.svg"}
+                    width={300}
+                    height={200}
+                  />
+                  <p className="text-white text-[16px] text-center font-bold">
+                    Skills
+                  </p>
+                  <div className="flex gap-x-10">
+                    <FaJsSquare className="bg-yellow-500 text-[36px]" />
+                    <FaHtml5 className="bg-orange-500 text-[36px]" />
+                    <FaCss3Alt className="bg-blue-600 text-[36px]" />
+                  </div>
+                </div>
+                <p className="text-white text-[16px] text-center">
+                  An organization for likeminded web developers who want to
+                  create amazing web projects. Taught workshops to students
+                  about HTML, CSS, and Javascript. Worked on projects that had
+                  impacts.
                 </p>
               </div>
             </div>
