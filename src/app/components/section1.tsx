0072eba2232ffefdbc2a2025a425ee506523c577
Wen Chen <wc467@cornell.edu>
2024-12-08 18:38:12 -0500

    reiterating

--- src/app/components/section1.tsx
+++ src/app/components/section1.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 function Section1() {
   return (
-    <div className="w-5/6 flex justify-center relative top-[10%]">
-      <div className="w-[70%] flex justify-center ">
+    <div className="flex justify-center relative top-[10%]">
+      <div className="w-full flex justify-center ">
         <div className="flex justify-around w-full">
-          <div className="">
+          <div className="w-2/3">
             <p className="text-[40px] text-center text-white font-sans font-semibold">
               Welcome
             </p>
@@ -16,19 +16,20 @@ function Section1() {
                 chess and Catan. I like running and playing pickleball.
               </p>
             </div>
-            {/* <div className="flex">
+            <div className="flex justify-center">
               <Image src={"/catan.png"} alt="catan" width={200} height={200} />
               <Image src={"/chess.webp"} alt="chess" width={200} height={200} />
-            </div> */}
+            </div>
           </div>
-
-          <Image
-            alt="image"
-            src={"/wenchen.jpg"}
-            width={200}
-            height={200}
-            className="rounded-full"
-          />
+          <div className="w-1/3">
+            <Image
+              alt="image"
+              src={"/wenchen.jpg"}
+              width={200}
+              height={200}
+              className="rounded-full"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,40 @@ export default function Home() {
         >
           <Section1 />
           <div className="flex items-center flex-col mt-[5%]">
-            <div className="w-4/5 min-h-screen" id="section-2">
+            <div>
+              <div className="w-full min-h-[300px]">
+                <p className="text-[32px] text-center text-white font-sans font-bold">
+                  Education
+                </p>
+                <div className="flex gap-x-10 mt-[5%]">
+                  <div className="flex flex-col gap-y-4 w-2/5">
+                    <Image
+                      alt="cornell"
+                      src={"/cornell.png"}
+                      width={300}
+                      height={200}
+                    />
+                  </div>
+                  <div className="flex flex-col w-full justify-center items-center">
+                    <p className="text-white text-[16px] text-center font-bold w-3/5">
+                      Expected Graduation: May 2025
+                    </p>
+                    <p className="text-white text-[16px] text-center font-bold w-3/5">
+                      Major: Computer Science
+                    </p>
+                    <p className="text-white text-[16px] text-center font-bold w-3/5">
+                      Minor: Applied Economics
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-4/5 min-h-fit" id="section-2">
               <p className="text-[32px] text-center text-white font-sans font-bold">
                 Work Experience
               </p>
               <div className="flex gap-x-10 mt-[5%]">
-                <div className="flex flex-col gap-y-4">
+                <div className="flex flex-col gap-y-4 w-2/5">
                   <Image
                     alt="experience 1"
                     src={"/gtrilogo.png"}
@@ -33,13 +61,13 @@ export default function Home() {
                     Skills
                   </p>
                   <div className="flex gap-x-10">
-                    <FaJsSquare className="bg-yellow-500 text-[36px]" />
-                    <SiTypescript className="bg-blue-500 text-[36px]" />
-                    <FaHtml5 className="bg-orange-500 text-[36px]" />
-                    <FaCss3Alt className="bg-blue-600 text-[36px]" />
+                    <FaJsSquare className="bg-yellow-500 text-[36px] text-black" />
+                    <SiTypescript className="bg-blue-500 text-[36px] text-black" />
+                    <FaHtml5 className="bg-orange-500 text-[36px] text-black" />
+                    <FaCss3Alt className="bg-blue-600 text-[36px] text-black" />
                   </div>
                 </div>
-                <p className="text-white text-[16px] text-center">
+                <p className="text-white text-[16px] text-center font-bold w-3/5">
                   Worked in the ELSYS division of Georgia Tech Research
                   Institute. Created and catered web interfaces for clients.
                 </p>
@@ -49,28 +77,57 @@ export default function Home() {
               <p className="text-[32px] text-center text-white font-sans font-bold">
                 Project Experience
               </p>
-              <div className="flex gap-x-10 mt-[5%]">
-                <div className="flex flex-col gap-y-4">
-                  <Image
-                    alt="experience 1"
-                    src={"/webdev.svg"}
-                    width={300}
-                    height={200}
-                  />
-                  <p className="text-white text-[16px] text-center font-bold">
-                    Skills
-                  </p>
-                  <div className="flex gap-x-10">
-                    <FaJsSquare className="bg-yellow-500 text-[36px] text-black" />
-                    <FaHtml5 className="bg-orange-500 text-[36px text-black" />
-                    <FaCss3Alt className="bg-blue-600 text-[36px] text-black" />
+              <div className="flex gap-x-10 mt-[5%] w-full">
+                <div className="w-2/5">
+                  <div className="flex flex-col gap-y-4">
+                    <Image
+                      alt="experience 1"
+                      src={"/webdev.svg"}
+                      width={300}
+                      height={200}
+                    />
+                    <p className="text-white text-[16px] text-center font-bold">
+                      Skills
+                    </p>
+                    <div className="flex gap-x-10">
+                      <SiTypescript className="bg-blue-500 text-[36px] text-black" />
+                      <FaHtml5 className="bg-orange-500 text-[36px] text-black" />
+                      <FaCss3Alt className="bg-blue-600 text-[36px] text-black" />
+                    </div>
                   </div>
                 </div>
-                <p className="text-white text-[16px] text-center">
+                <p className="text-white text-[16px] text-center font-semibold w-3/5">
                   An organization for likeminded web developers who want to
                   create amazing web projects. Taught workshops to students
                   about HTML, CSS, and Javascript. Worked on projects that
                   benefited the Cornell community.
+                </p>
+              </div>
+              <div className="flex gap-x-10 mt-[5%] w-full">
+                <div className="w-2/5">
+                  <div className="flex flex-col gap-y-4">
+                    <Image
+                      alt="experience 1"
+                      src={"/roomie.png"}
+                      width={300}
+                      height={200}
+                    />
+                    <p className="text-white text-[16px] text-center font-bold">
+                      Skills
+                    </p>
+                    <div className="flex gap-x-10">
+                      <FaJsSquare className="bg-yellow-500 text-[36px] text-black" />
+                      <FaHtml5 className="bg-orange-500 text-[36px] text-black" />
+                      <FaCss3Alt className="bg-blue-600 text-[36px] text-black" />
+                    </div>
+                  </div>
+                </div>
+                <p className="text-white text-[16px] text-center font-semibold w-3/5">
+                  Worked on a roommate matching service to match Cornell
+                  students who are looking for roommates and housing in one
+                  place. Utilizing Supabase, NodeJs, and ExpressJs to handle
+                  server calls and database requests, and Javascript, HTML, and
+                  TailwindCSS for frontend
                 </p>
               </div>
             </div>
